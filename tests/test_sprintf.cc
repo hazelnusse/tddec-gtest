66628dd7b2c9136c3c0261b86b7896a06b8b83ec
Dale Lukas Peterson <hazelnusse@gmail.com>
2014-09-27 17:35:49 -0700

    Add Test_sprintf InsertString test

--- tests/test_sprintf.cc
+++ tests/test_sprintf.cc
@@ -13,3 +13,11 @@ TEST_F(Test_sprintf, NoFormatOperations) {
     ASSERT_STREQ("hey", output);
 }
 
+TEST_F(Test_sprintf, InsertString) {
+
+    char output[20] = "";
+
+    ASSERT_EQ(12, sprintf(output, "Hello %s\n", "World"));
+    ASSERT_STREQ("Hello World\n", output);
+}
+
