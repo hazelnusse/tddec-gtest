db7db734cc8950fd012aec177cf7a3a0f89adad2
Dale Lukas Peterson <hazelnusse@gmail.com>
2014-09-28 15:01:57 -0700

    Implement TurnOnLedOne test

--- tests/LedDriver.h
+++ tests/LedDriver.h
@@ -5,10 +5,15 @@
 
 class LedDriver {
 public:
-    LedDriver(uint16_t& address) {
-        address = 0;
+    LedDriver(uint16_t& address) : m_address{address} {
+        m_address = 0;
+    }
+
+    void TurnOn(int ledNumber) {
+        m_address = 1;
     }
 private:
+    uint16_t& m_address;
 };
 
 #endif
--- tests/test_LedDriver.cc
+++ tests/test_LedDriver.cc
@@ -17,3 +17,8 @@ TEST_F(Test_LedDriver, LedsOffAfterCreate) {
     ASSERT_EQ(0, virtualLeds);
 }
 
+TEST_F(Test_LedDriver, TurnOnLedOne) {
+    led.TurnOn(1);
+    ASSERT_EQ(1, virtualLeds);
+}
+
